73c1f8689cb218aa0b290a9efd7d6ea61b52cf8a
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 11:07:06 -0500

    Add a Header form section to the resume form

--- src/components/form/ResumeForm.jsx
+++ src/components/form/ResumeForm.jsx
@@ -1,5 +1,10 @@
 import "../../styles/form/ResumeForm.css";
+import FormSection from "./FormSection";
 
 export default function ResumeForm() {
-  return <div className="resume-form"></div>;
+  return (
+    <div className="resume-form">
+      <FormSection name="Header"></FormSection>
+    </div>
+  );
 }
