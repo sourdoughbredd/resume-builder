92ee74fd53e51cac8889357c35a774f1941558b8
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 09:15:50 -0500

    Create basic work experience component

---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,31 @@
+import "../styles/WorkExperience.css";
+import Bullet from "./Bullet";
+
+export default function WorkExperience({
+  title,
+  employer,
+  start,
+  end,
+  responsibilities,
+}) {
+  return (
+    <div className="work-experience">
+      <div className="work-header">
+        <Bullet></Bullet>
+        <div className="work-title">
+          <p>
+            <strong>{title},</strong> {employer}
+          </p>
+        </div>
+        <div className="work-date">
+          {start} - {end}
+        </div>
+      </div>
+      <ul className="work-details">
+        {responsibilities.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
