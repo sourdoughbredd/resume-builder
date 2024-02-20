38673b52ae5c35375276de132c0af59f691aee08
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 12:01:43 -0500

    Add resume info state to pass between form and preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import "./App.css";
 import Resume from "./components/preview/Resume.jsx";
 import ResumeForm from "./components/form/ResumeForm.jsx";
+import { useState } from "react";
+
+const initialResumeInfo = {
+  Header: {},
+  Education: {},
+  Skillsets: {},
+  WorkExperience: {},
+};
 
 function App() {
+  const [resumeInfo, setResumeInfo] = useState(initialResumeInfo);
   return (
     <div className="app-container">
-      <ResumeForm></ResumeForm>
-      <Resume></Resume>
+      <ResumeForm
+        resumeInfo={resumeInfo}
+        setResumeInfo={setResumeInfo}
+      ></ResumeForm>
+      <Resume resumeInfo={resumeInfo}></Resume>
     </div>
   );
 }
