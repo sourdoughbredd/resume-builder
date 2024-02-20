6427672cc82bce4c708844ee900e5de29a8bfec3
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 07:59:20 -0500

    Add some Education components to resume preview

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,7 @@
 import "../styles/Resume.css";
 import Header from "./Header";
 import Section from "./Section";
+import Education from "./Education";
 
 export default function Resume() {
   return (
@@ -11,7 +12,38 @@ export default function Resume() {
         email="bwbussell24@gmail.com"
         website="www.linkedin.com/in/brett-bussell"
       ></Header>
-      <Section name="Education"></Section>
+      <Section name="Education">
+        <Education
+          degree="M.S. in Mechanical Engineering"
+          school="University of California, Berkeley"
+          date="05/2021"
+          details={[
+            { title: "GPA", description: "3.77/4.00" },
+            {
+              title: "Emphasis",
+              description: "Modeling and control of dynamical systems",
+            },
+            {
+              title: "Select Coursework",
+              description: "Control of Unmanned Aerial Vehicles",
+            },
+          ]}
+        ></Education>
+        <Education
+          degree="B.S. in Mechanical Engineering"
+          school="University of California, Berkeley"
+          date="12/2019"
+          details={[
+            { title: "GPA", description: "3.90/4.00, Graduation with Honors" },
+            {
+              title: "Select Coursework",
+              description: "Lagrangian Dynamics, Advanced Linear Algebra",
+            },
+          ]}
+          gpa="3.90/4.00, Graduation with Honors"
+          courses="Lagrangian Dynamics, Advanced Linear Algebra"
+        ></Education>
+      </Section>
       <Section name="Work Experience"></Section>
     </div>
   );
