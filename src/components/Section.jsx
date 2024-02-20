b4a58f713deae17641dc89a64b2899cf118f58e2
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 09:59:04 -0500

    Enclose children in div so they can be styled with css selector

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,7 +5,7 @@ export default function Section({ name, children }) {
     <div className="section">
       <h3>{name}</h3>
       <hr />
-      {children}
+      <div>{children}</div>
     </div>
   );
 }
