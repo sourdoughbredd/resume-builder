e582140b6aaade764a7eba4f52b4c51e9218425e
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 12:02:38 -0500

    Fill firs tand last name with resume info state

--- src/components/form/HeaderForm.jsx
+++ src/components/form/HeaderForm.jsx
@@ -1,4 +1,19 @@
-export default function HeaderForm() {
+export default function HeaderForm({ resumeInfo, setResumeInfo }) {
+  function nameChanged(e) {
+    let newResumeInfo = { ...resumeInfo };
+    let oldFirst = "";
+    let oldLast = "";
+    if (newResumeInfo.Header.name) {
+      [oldFirst, oldLast] = newResumeInfo.Header.name.split(" ");
+    }
+    if (e.target.id === "firstname") {
+      newResumeInfo.Header.name = e.target.value + " " + oldLast;
+    } else {
+      newResumeInfo.Header.name = oldFirst + " " + e.target.value;
+    }
+    setResumeInfo(newResumeInfo);
+  }
+
   return (
     <form className="header-form">
       <input
@@ -6,12 +21,14 @@ export default function HeaderForm() {
         name="firstname"
         id="firstname"
         placeholder="First Name"
+        onChange={nameChanged}
       />
       <input
         type="text"
         name="lastname"
         id="lastname"
         placeholder="Last Name"
+        onChange={nameChanged}
       />
       <input type="tel" name="phone" id="phone" placeholder="Phone Number" />
       <input type="email" name="email" id="email" placeholder="Email" />
