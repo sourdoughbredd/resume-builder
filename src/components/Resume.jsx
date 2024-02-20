76bc73eb854a908351a931ada76c8aad2c118084
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 09:15:06 -0500

    Add some work experience components to resume preview

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,6 +2,7 @@ import "../styles/Resume.css";
 import Header from "./Header";
 import Section from "./Section";
 import Education from "./Education";
+import WorkExperience from "./WorkExperience";
 
 export default function Resume() {
   return (
@@ -44,7 +45,68 @@ export default function Resume() {
           courses="Lagrangian Dynamics, Advanced Linear Algebra"
         ></Education>
       </Section>
-      <Section name="Work Experience"></Section>
+      <Section name="Work Experience">
+        <WorkExperience
+          title="Flight Controls Engineer (F/A-18)"
+          employer="Naval Air Systems Command (NAVAIR)"
+          start="10/2021"
+          end="07/2023"
+          responsibilities={[
+            {
+              id: 0,
+              text: "Developed and maintained MATLAB scripts for aggregation and analysis of 400,000 Monte Carlo simulation runs, ensuring control laws meet requirements",
+            },
+            {
+              id: 1,
+              text: "Boosted Monte Carlo data aggregation rate by 6x using parallel processing, saving hundreds of hours of computation time and reducing code development timeline",
+            },
+            {
+              id: 2,
+              text: "Identified and resolved a small but critical design error in the control law Simulink model, preventing potential losses of millions of testing dollars and ensuring the project’s timely success",
+            },
+            {
+              id: 3,
+              text: "Refined control laws and analysis methodologies through targeted SWIL analyses using high-fidelity CASTLE simulation software and MATLAB/Simulink",
+            },
+            {
+              id: 4,
+              text: "Supported control law testing and validation during HWIL/PIL (pilot-in-the-loop) sim sessions at the state-of-the-art F-18 simulators at the Navy’s Manned Flight Simulator facility",
+            },
+          ]}
+        ></WorkExperience>
+        <WorkExperience
+          title="Instructor"
+          employer="UC Berkeley College of Engineering, T-PREP Program"
+          start="08/2020"
+          end="08/2021"
+          responsibilities={[
+            {
+              id: 0,
+              text: "Taught a 3 week crash course in MATLAB programming to a group of 30 incoming transfer students",
+            },
+            {
+              id: 1,
+              text: "Created a set of university-level lecture materials, including 12 lectures and live programming demos",
+            },
+          ]}
+        ></WorkExperience>
+        <WorkExperience
+          title="Student Researcher"
+          employer="UC Berkeley Hybrid Robotics Lab"
+          start="03/2018"
+          end="12/2019"
+          responsibilities={[
+            {
+              id: 0,
+              text: "Built a simulation of nonlinear vehicle dynamics (based on 3-state bicycle model) in MATLAB Simulink",
+            },
+            {
+              id: 1,
+              text: "Designed a cascaded PID control system that can autonomously drift at desired radii with varying initial conditions",
+            },
+          ]}
+        ></WorkExperience>
+      </Section>
     </div>
   );
 }
