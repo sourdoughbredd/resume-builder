5182f8acf79d26c471a080da9af4cd9d7c7ca8f0
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-20 12:02:52 -0500

    Pass resumeInfo state to HeaderForm

--- src/components/form/ResumeForm.jsx
+++ src/components/form/ResumeForm.jsx
@@ -2,11 +2,14 @@ import "../../styles/form/ResumeForm.css";
 import FormSection from "./FormSection";
 import HeaderForm from "./HeaderForm";
 
-export default function ResumeForm() {
+export default function ResumeForm({ resumeInfo, setResumeInfo }) {
   return (
     <div className="resume-form">
       <FormSection name="Header">
-        <HeaderForm></HeaderForm>
+        <HeaderForm
+          resumeInfo={resumeInfo}
+          setResumeInfo={setResumeInfo}
+        ></HeaderForm>
       </FormSection>
     </div>
   );
