8d3240ded188629d18315d64c812d381b252441c
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-22 10:06:12 -0500

    Move initialResumeInfo object to own script

---
+++ src/initialResumeInfo.js
@@ -0,0 +1,45 @@
+export const initialResumeInfo = {
+  Header: {
+    name: "Brett Bussell",
+    phone: "(209) 585 5343",
+    email: "bwbussell24@gmail.com",
+    website: "www.linkedin.com",
+  },
+  Education: [
+    {
+      id: 0,
+      degree: "M.S. in Mechanical Engineering",
+      school: "University of California, Berkeley",
+      date: "05/2021",
+      extras: [
+        { id: 0, title: "GPA", description: "3.7/4.0" },
+        {
+          id: 1,
+          title: "Emphasis",
+          description: "Modeling and control of dynamical systems",
+        },
+        {
+          id: 2,
+          title: "Select Coursework",
+          description: "Control of Unmanned Aerial Vehicles",
+        },
+      ],
+    },
+    {
+      id: 1,
+      degree: "B.S. in Mechanincal Engineering",
+      school: "University of California, Berkeley",
+      date: "12/2019",
+      extras: [
+        { id: 0, title: "GPA", description: "3.9/4.0" },
+        {
+          id: 1,
+          title: "Select Coursework",
+          description: "Lagrangian Dynamics, Advanced Linear Algebra",
+        },
+      ],
+    },
+  ],
+  Skillsets: {},
+  WorkExperience: {},
+};
