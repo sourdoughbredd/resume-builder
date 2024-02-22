43ad77fd607c50533ea2f0c50b881f1b78ab171c
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-22 10:05:57 -0500

    Import initialResumeInfo object from its own script

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import "./App.css";
 import Resume from "./components/preview/Resume.jsx";
 import ResumeForm from "./components/form/ResumeForm.jsx";
 import { useState } from "react";
-
-const initialResumeInfo = {
-  Header: {},
-  Education: {},
-  Skillsets: {},
-  WorkExperience: {},
-};
+import { initialResumeInfo } from "./initialResumeInfo.js";
 
 function App() {
   const [resumeInfo, setResumeInfo] = useState(initialResumeInfo);
