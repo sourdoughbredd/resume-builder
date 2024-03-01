be3f3772296af7895958a2274e39d7d14ab6e45e
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-03-01 07:55:51 -0500

    import styles

--- src/components/form/SkillsetsForm.jsx
+++ src/components/form/SkillsetsForm.jsx
@@ -1,3 +1,5 @@
+import "../../styles/form/SkillsetsForm.css";
+
 export default function SkillsetsForm({ skillsetsInfo, setSkillsetsInfo }) {
   function nameChanged(e, id) {
     const newSkillsetsInfo = [...skillsetsInfo];
